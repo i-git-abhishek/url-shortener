c763c59da13cf95b5344821be2c3bbe135bc7d48
i-git-abhishek <ggolu1701@gmail.com>
2026-09-03 11:42:19 +0530

    relax max login attempts

--- Backend/app.js
+++ Backend/app.js
@@ -14,6 +14,7 @@ import cookieParser from "cookie-parser";
 dotenv.config("./.env");
 const app = express();
 const port = process.env.PORT || 3000;
+app.set("trust proxy", 1);
 
 
 app.use(
--- Backend/src/middleware/rateLimit.middleware.js
+++ Backend/src/middleware/rateLimit.middleware.js
@@ -2,7 +2,7 @@ import {rateLimit} from "express-rate-limit";
 
 export const generalIpLimiter = rateLimit({
     windowMs: 15*60*1000,
-    max: 15,
+    max: 100,
     message: { message: "Too many requests from this IP, please try again after 15 minutes." }, standardHeaders: true,
     legacyHeaders: false,
 });
